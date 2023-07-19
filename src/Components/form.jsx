c9f74969a3c7f9fc12946320d4cd717453533958
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-19 09:05:08 +0430

    Category id renamed

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -3,8 +3,8 @@ function Form() {
     <form>
       <input type="text" placeholder="Book Title" required />
       <input type="text" placeholder="Book Author" required />
-      <select name="catagories" id="form-catagory">
-        <option value="catagory">Catagory</option>
+      <select name="categories" id="form-category">
+        <option value="category">Category</option>
       </select>
       <button type="submit" className="book-buttons">ADD BOOK</button>
     </form>
