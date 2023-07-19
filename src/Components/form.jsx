fdb73574e0d8cd1175188f14d49353c1b8a3eebe
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-19 08:53:50 +0430

    The Select tag added

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -3,6 +3,9 @@ function Form() {
     <form>
       <input type="text" placeholder="Book Title" required />
       <input type="text" placeholder="Book Author" required />
+      <select name="catagories" id="form-catagory">
+        <option value="catagory">Catagory</option>
+      </select>
       <button type="submit" className="book-buttons">ADD BOOK</button>
     </form>
   );
